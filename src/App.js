e8c29c63a78fa656601bb61deea532695787c488
Jagmohan Dixit <78152773+Jagmohan-Dixit@users.noreply.github.com>
2021-08-08 14:17:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+// import Nav1 from './components/basic project1/nav1';
+// import './App.css';
+// import Api from './components/basic project1/Api';
+// import UseState from './components/project1_incdec/useState';
+// import UseEffect from './components/project1_incdec/useEffect';
+// import UseReducer from './components/project1_incdec/useReducer';
+
+// import Todo from './components/project2_todo_list/todo';
+
+// import Events from './components/project3_events_handling/events';
+
+// import Showdata from './components/basic project1/Showdata';
+
+import Api from './components/Api_project/Api';
+
+
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+    {/* <Showdata />  */}
+    <Api />
+    </div>
+  ) 
+}
+
+export default App;
